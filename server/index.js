88fb2c01232b55e6de18e34d1df3fa89975077a7
Muhammad Qasim <muhammadqasim.sahil@gmail.com>
2025-06-20 00:50:57 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // important!
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
